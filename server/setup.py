cd3cab426d224661547a5506363433899e8876e9
wsl-user <balintataw@gmail.com>
2019-12-11 09:32:08 -0800

    Init

---
+++ server/setup.py
@@ -0,0 +1,16 @@
+"""
+This is to define the project so that it's packages can be installed.
+
+https://docs.python.org/3.8/distutils/setupscript.html
+"""
+
+from distutils.core import setup
+
+setup(name='app-server',
+      version='1.0',
+      description='Virtual Game Controller',
+      author='Kevin Jossendal',
+      author_email='kjossendal@gmail.com',
+      url='https://github.com/aenglander/flask-in-a-flash',
+      packages=['server'],
+      )
